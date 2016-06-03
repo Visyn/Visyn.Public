74f15709579a4ed27ebf47c01b5cac48df523eab
randlee <github@randlee.com>
2016-06-03 15:36:09 -0700

    Visyn.Cef.Wpf basic browser working Visyn.Public portable base library created

---
+++ Settings/CefSettings.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Visyn.Public.VisynApp;
+
+namespace Visyn.Public.Settings
+{
+    public class CefAppSettings : IVisynAppSettings
+    {
+        public bool AreValid { get; protected set; } = true;
+
+        public bool AddressBarVisible { get; }
+
+        public string Url { get; }
+
+        public CefAppSettings(string url, bool addressBarVisible=false)
+        {
+            AddressBarVisible = addressBarVisible;
+            Url = url;
+        }
+
+        public static CefAppSettings DefaultSettings()
+        {
+            return new CefAppSettings("www.yahoo.com", false);
+        }
+    }
+}
---
+++ VisynApp/IVisynApp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Visyn.Public.VisynApp
+{
+    /// <summary>
+    /// Interface IVisynApp
+    /// </summary>
+    public interface IVisynApp
+    {
+    }
+}
