1e5c690cd24f6632b720de9eaf44ee8b21d4e8d1
randlee <github@randlee.com>
2016-06-03 15:36:09 -0700

    Visyn.Cef.Wpf basic browser working Visyn.Public portable base library created

---
+++ CommandLine/CommandLineArgs.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace Visyn.Public.CommandLine
+{
+    /// <summary>
+    /// Class CommandLineArgs base class called from all applications.
+    /// </summary>
+    /// <seealso cref="Visyn.Public.CommandLine.ICommandLineArgs" />
+    public class CommandLineArgs : ICommandLineArgs
+    {
+        /// <summary>
+        /// Try to parse command line arguments
+        /// </summary>
+        /// <param name="args">The arguments to parse.</param>
+        /// <param name="result">The parsed result if successful.  Null if parse failed.</param>
+        /// <returns><c>true</c> if arguments were successfuly parsed, <c>false</c> otherwise.</returns>
+        public virtual bool TryParse(string[] args, out ICommandLineArgs result)
+        {
+            if (args == null || args.Length == 0)
+            {
+                result = null;
+                return false;
+            }
+            result = Parse(args);
+            return result != null;
+        }
+
+        /// <summary>
+        /// Try to parse command line arguments as a specific type
+        /// </summary>
+        /// <typeparam name="T">Type to parse as</typeparam>
+        /// <param name="args">The arguments to parse.</param>
+        /// <param name="result">The parsed result if successful.  null if parse failed.</param>
+        /// <returns><c>true</c> if arguments were successfuly parsed, <c>false</c> otherwise.</returns>
+        public virtual bool TryParse<T>(string[] args, out T result) where T : class
+        {
+            result = null;
+            if (args == null) return false;
+
+            result = ParseAs<T>(args);
+            return (result != null);
+        }
+
+        /// <summary>
+        /// Try to parse command line arguments
+        /// </summary>
+        /// <param name="args">The arguments to parse.</param>
+        /// <returns>The parsed result if successful.  Null if parse failed.</returns>
+        public static ICommandLineArgs Parse(string[] args)
+        {
+            if (args == null) return null;
+            return null;
+        }
+
+        /// <summary>
+        /// Try to parse command line arguments as a specific type
+        /// </summary>
+        /// <typeparam name="T">Type to parse as</typeparam>
+        /// <param name="args">The arguments to parse.</param>
+        /// <returns>The parsed result if successful.  null if parse failed.</returns>
+        public static T ParseAs<T>(string[] args) where T : class
+        {
+            if (args == null) return null;
+            if (typeof(T) is ICommandLineArgs)
+            {
+                try
+                {
+                    var parser = Activator.CreateInstance(typeof(T)) as ICommandLineArgs;
+                    if (parser != null)
+                    {
+                        T result;
+                        parser.TryParse<T>(args, out result);
+                        return result;
+                    }
+                }
+                catch(Exception exc)
+                {
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ CommandLine/ICommandLineArgs.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Visyn.Public.CommandLine
+{
+    /// <summary>
+    /// Interface ICommandLineArgs
+    /// Interface for defining and parsing command line arguments
+    /// </summary>
+    public interface ICommandLineArgs
+    {
+        /// <summary>
+        /// Try to parse command line arguments
+        /// </summary>
+        /// <param name="args">The arguments to parse.</param>
+        /// <param name="result">The parsed result if successful.  Null if parse failed.</param>
+        /// <returns><c>true</c> if arguments were successfuly parsed, <c>false</c> otherwise.</returns>
+        bool TryParse(string[] args, out ICommandLineArgs result);
+
+        /// <summary>
+        /// Try to parse command line arguments as a specific type
+        /// </summary>
+        /// <typeparam name="T">Type to parse as</typeparam>
+        /// <param name="args">The arguments to parse.</param>
+        /// <param name="result">The parsed result if successful.  null if parse failed.</param>
+        /// <returns><c>true</c> if arguments were successfuly parsed, <c>false</c> otherwise.</returns>
+        bool TryParse<T>(string[] args, out T result) where T : class;
+    }
+}
