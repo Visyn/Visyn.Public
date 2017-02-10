70b6a44a9c521c567fe8caadf6871eea65884111
randlee <github@randlee.com>
2017-02-09 18:02:54 -0800

    clean-up

---
+++ Types/Time/DateTimeExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Visyn.Public.Types.Time
+{
+    public static class DateTimeExtensions
+    { 
+        public static DateTime Floor(this DateTime date, TimeSpan interval)
+        {
+            return date.AddTicks(-(date.Ticks % interval.Ticks));
+        }
+
+        public static DateTime Ceiling(this DateTime date, TimeSpan interval)
+        {
+            var overflow = date.Ticks % interval.Ticks;
+
+            return overflow == 0 ? date : date.AddTicks(interval.Ticks - overflow);
+        }
+
+        public static DateTime Round(this DateTime date, TimeSpan interval)
+        {
+            var halfIntervelTicks = (interval.Ticks + 1) >> 1;
+
+            return date.AddTicks(halfIntervelTicks - ((date.Ticks + halfIntervelTicks) % interval.Ticks));
+        }
+        
+        public static TimeSpan TimeSince1970(this DateTime date) => date - new DateTime(1970,1,1,0,0,0);
+
+        public static double MillisecondsSince1970(this DateTime date) => (date - new DateTime(1970, 1, 1, 0, 0, 0)).TotalMilliseconds;
+
+        public static DateTime FromMillisecondsSince1970(this double milliseconds) => new DateTime(1970,1,1,0,0,0).AddMilliseconds(milliseconds);
+    }
+}
---
+++ Types/Time/TimeSpanExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Visyn.Public.Types.Time
+{
+    public static class TimeSpanExtensions
+    { 
+        public static TimeSpan Floor(this TimeSpan span, TimeSpan interval)
+        {
+            return new TimeSpan(span.Ticks - (span.Ticks % interval.Ticks));
+        }
+
+        public static TimeSpan Ceiling(this TimeSpan span, TimeSpan interval)
+        {
+            var overflow = span.Ticks % interval.Ticks;
+
+            return overflow == 0 ? span : new TimeSpan(span.Ticks  + (interval.Ticks - overflow));
+        }
+
+        public static TimeSpan Round(this TimeSpan span, TimeSpan interval)
+        {
+            var halfIntervelTicks = (interval.Ticks + 1) >> 1;
+
+            return new TimeSpan(span.Ticks + (halfIntervelTicks - ((span.Ticks + halfIntervelTicks) % interval.Ticks)));
+        }
+
+        public static TimeSpan Multiply(this TimeSpan span, double multiply)
+        {
+            var ticks = span.Ticks*multiply;
+            if (ticks >= TimeSpan.MaxValue.Ticks) return TimeSpan.MaxValue;
+            if (ticks <= TimeSpan.MinValue.Ticks) return TimeSpan.MinValue;
+            return TimeSpan.FromTicks((long)ticks);
+        }
+
+        public static TimeSpan Divide(this TimeSpan span, double divisor)
+        {
+            var ticks = span.Ticks / divisor;
+            if (ticks >= TimeSpan.MaxValue.Ticks) return TimeSpan.MaxValue;
+            if (ticks <= TimeSpan.MinValue.Ticks) return TimeSpan.MinValue;
+            return TimeSpan.FromTicks((long)ticks);
+        }
+    }
+}
