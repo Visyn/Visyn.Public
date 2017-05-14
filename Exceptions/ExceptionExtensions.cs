7734824fe83cacad021c94d4ee0a0f2690446120
randlee <github@randlee.com>
2017-05-13 17:41:49 -0700

    intermediate - broken check-in

--- Exceptions/ExceptionExtensions.cs
+++ Exceptions/ExceptionExtensions.cs
@@ -33,6 +33,7 @@ namespace Visyn.Public.Exceptions
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        [Obsolete("Misleading name!  Supress should not throw under any conditon", true)]
         public static void Supress<TExc>(this IExceptionHandler handler, Action action) 
             where TExc : Exception
         {
@@ -48,6 +49,7 @@ namespace Visyn.Public.Exceptions
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        [Obsolete("Misleading name!  Supress should not throw under any conditon", true)]
         public static TOut Supress<TExc,TOut>(this IExceptionHandler handler, Func<TOut> funct)
             where TExc : Exception
         {
@@ -63,8 +65,26 @@ namespace Visyn.Public.Exceptions
             return default(TOut);
         }
 
+        [Obsolete("Not used",true)]
         public static bool SupressException(this IExceptionHandler handler, object sender, Exception exc) => true;
 
         public static bool SupressException(object sender, Exception exc) => true;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool VerifyNotNullOrThrow(this IExceptionHandler handler, object itemToCheck,object sender, string message)
+        {
+            if (itemToCheck != null) return true;
+            var nullReferenceException = new NullReferenceException(message);
+
+            if (handler?.HandleException(sender, nullReferenceException) != true) throw nullReferenceException;
+            return false;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool VerifyNotNullOrThrow(this object itemToCheck, string message)
+        {
+            if (itemToCheck != null) return true;
+            throw  new NullReferenceException(message);
+        }
     }
 }
