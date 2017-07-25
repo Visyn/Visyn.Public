01ef4a9ace4a1fdde382d41a1142b42cb5b5ae0b
randlee <github@randlee.com>
2017-07-25 11:05:21 -0700

    Minor improvements

---
+++ Io/OutputDevice.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace Visyn.Public.Io
+{
+    public static class OutputDevice
+    {
+        public static void Write(this IOutputDevice output, IEnumerable<string> lines)
+        {
+            var multiline = output as IOutputDeviceMultiline;
+            if (multiline != null)
+                multiline.Write(lines);
+            else
+                foreach (var line in lines) output.WriteLine(line);
+        }
+    }
+}
