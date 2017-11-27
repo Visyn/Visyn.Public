366fa8f92b902864c9a149e4d8b3881077054f58
randlee <github@randlee.com>
2017-11-27 14:41:12 -0800

    Copyright notice update

--- Lib/Properties/AssemblyInfo.cs
+++ Lib/Properties/AssemblyInfo.cs
@@ -1,4 +1,28 @@
-﻿using System.Reflection;
+#region Copyright (c) 2015-2017 Visyn
+// The MIT License(MIT)
+// 
+// Copyright(c) 2015-2017 Visyn
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+#endregion
+
+using System.Reflection;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -7,7 +31,7 @@
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyProduct("Visyn.Public")]
 [assembly: AssemblyCompany("Visyn")]
-[assembly: AssemblyCopyright("Copyright © 2015-2017")]
+[assembly: AssemblyCopyright("Copyright � 2015-2017")]
 [assembly: AssemblyTrademark("Visyn")]
 [assembly: AssemblyCulture("")]
 #if DEBUG
