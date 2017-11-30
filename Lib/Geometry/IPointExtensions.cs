d30bc514b25ad5c6df994644142e523b31cec5c4
randlee <github@randlee.com>
2017-11-30 10:58:15 -0800

    IPointExtensions and ValueStore

---
+++ Lib/Geometry/IPointExtensions.cs
@@ -0,0 +1,11 @@
+﻿
+using System.Windows;
+
+namespace Visyn.Geometry
+{
+    public static class IPointExtensions
+    {
+        public static Point ToPoint(this IPoint point) => new Point(point.X,point.Y);
+        public static PointXY ToPointXY(this IPoint point) => new PointXY(point.X, point.Y);
+    }
+}
