e29b9277e65d1c6f4a3d565e3306ae2b0f11c31b
Rand Lee <github@randlee.com>
2018-03-26 13:21:21 -0700

    Removed obsolete files.

--- Lib/Properties/AssemblyInfo.cs
+++ Lib/Properties/AssemblyInfo.cs
@@ -52,5 +52,5 @@ using System.Resources;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.1.82.118")]
-[assembly: AssemblyFileVersion("2.1.82.118")]
+[assembly: AssemblyVersion("2.1.85.121")]
+[assembly: AssemblyFileVersion("2.1.85.121")]
